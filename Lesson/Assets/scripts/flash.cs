44ab37168d2f44d9ea35196d1d4f38cb714f43bd
Hafizh Subhan Mahdiy <hafizhkerabat1@gmail.com>
2017-04-09 14:27:51 +0700

    The Flash of Smokie!!!

---
+++ Lesson/Assets/scripts/flash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class flash : MonoBehaviour {
+
+	public Animator kilau;
+	public bool grab;
+
+	// Use this for initialization
+	void Start () {
+		kilau = GetComponent<Animator> ();
+		grab = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (grab) {
+			kilau.SetBool ("grab", true);
+			grab = false;
+		} else {
+			kilau.SetBool ("grab", false);
+		}
+	}
+}
