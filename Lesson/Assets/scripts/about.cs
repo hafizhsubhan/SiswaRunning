d203276a937b980e77f95c1990fcd2f267450f9c
Hafizh Subhan Mahdiy <hafizhkerabat1@gmail.com>
2017-04-11 07:03:07 +0700

    About page Remake

---
+++ Lesson/Assets/scripts/about.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class about : MonoBehaviour {
+
+	public void back(){
+		SceneManager.LoadScene ("menu");
+	}
+}
